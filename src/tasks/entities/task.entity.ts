3af8541814b6c14dcde75907645954d1a808f0e3
Anderson Coelho <anderson.coelho@conecthus.org.br>
2026-04-14 16:48:44 -0400

    feat: create tasks resource and entity

---
+++ src/tasks/entities/task.entity.ts
@@ -0,0 +1,38 @@
+import { BeforeInsert, Column, CreateDateColumn, Entity, ManyToOne, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { User } from "../../users/entities/user.entity";
+import { nanoid } from "nanoid";
+
+@Entity('tasks')
+export class Task {
+    @PrimaryColumn()
+    id!: string;
+
+    @Column()
+    title!: string
+
+    @Column({ nullable: true })
+    description!: string
+
+    @Column({ default: false })
+    completed!: string;
+
+    @Column({ nullable: false })
+    dueDate!: string;
+
+    @ManyToOne(() => User, (user) => user.tasks, { onDelete: 'CASCADE'})
+    user!: User;
+
+    @Column()
+    userId!: string;
+
+    @CreateDateColumn()
+    createAt!: Date;
+
+    @UpdateDateColumn()
+    updatedAt!: Date;
+
+    @BeforeInsert()
+    generateId() {
+        this.id = `task_${nanoid()}`
+    }
+}

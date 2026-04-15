b34502b23f127ce6d03b03a33c1fe25f12cf8704
Anderson Coelho <anderson.coelho@conecthus.org.br>
2026-04-15 09:33:10 -0400

    feat: tasks service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
+import { Repository } from 'typeorm';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    constructor(
+        @InjectRepository(Task)
+        private taskRepository: Repository<Task>
+    ) {}
+
+    async create(userId: string, dto: CreateTaskDto): Promise<Task> {
+        const task = this.taskRepository.create({
+            ...CreateTaskDto,
+            userId,
+        });
+        return this.taskRepository.save(task);
+    }
+
+    async findAllByUser(userId: string): Promise<Task[]> {
+        return this.taskRepository.find({
+            where: { userId },
+            order: { createAt: 'DESC'},
+        });
+    }
+
+    async findOne(id: string, userId: string): Promise<Task> {
+        const task = await this.taskRepository.findOne({
+            where: { id, userId },
+        });
+
+        if (!task) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+
+        return task;
+    }
+
+    async update(id: string, userId: string, updateTaskDto: UpdateTaskDto): Promise<Task> {
+        const task = await this.findOne(id, userId);
+
+        Object.assign(task, updateTaskDto);
+
+        return this.taskRepository.save(task);
+    }
+
+    async remove(id: string, userId: string): Promise<void> {
+        const result = await this.taskRepository.delete({ id, userId});
+
+        if (result.affected === 0) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+    }
+
+    async tooggleComplete(id: string, userId: string): Promise<Task> {
+        const task = await this.findOne(id, userId);
+
+        task.completed = !task.completed;
+
+        return this.taskRepository.save(task);
+    }
+}

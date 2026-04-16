053fb49e05fc4db26f59ab850957feec13f580fe
Anderson Coelho <anderson.coelho@conecthus.org.br>
2026-04-16 08:34:37 -0400

    chore: move guards and strategies to properly locations

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,18 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
+    constructor(configService: ConfigService) {
+        const secret = configService.get<string>('JWT_SECRET');
+
+        if (!secret) {
+            throw new Error('JWT_SECRET not configured. ')
+        }
+
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: 'SUPER_SECRET_KEY',
+            secretOrKey: secret,
         });
     }
 
     async validate(payload: any) {
-        return { userId: payload.sub, email: payload.email };
+        console.log("JWT Payload: ", payload); // TODO: Remove
+
+        return { id: payload.sub, email: payload.email };
     }
 }
--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { AuthService } from "./auth.service";
 import { Strategy } from "passport-local";
+import { AuthService } from "../auth.service";
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -12,8 +12,8 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     async validate(email: string, password: string): Promise<any> {
         const user = await this.authService.validateUser(email, password);
         if (!user) {
-            throw new UnauthorizedException();
+            throw new UnauthorizedException('Invalid authorization');
         }
         return user;
     }
- }
+}
